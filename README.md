68e24b9c56a5c0265236864a09194fe97fa24361
fatmeat <pathofyggdrasil@gmail.com>
2025-04-17 10:16:29 +0200

    table specification idea

---
+++ README.md
@@ -0,0 +1,4 @@
+## REF
+	https://en.wikipedia.org/wiki/Gap_buffer
+	https://en.wikipedia.org/wiki/Piece_table
+	https://en.wikipedia.org/wiki/Rope_(data_structure)
---
+++ source/editor/input.c
@@ -0,0 +1,22 @@
+#include "editor.h"
+
+int input(int mode) {
+	switch (mode) {
+		case (1): {
+			break;
+		}
+		case (2): {
+			break;
+		}
+		case (3): {
+			break;
+		}
+		case (4): {
+			break;
+		}
+		default: {
+			break;
+		}
+	}
+	return (mode);
+}
